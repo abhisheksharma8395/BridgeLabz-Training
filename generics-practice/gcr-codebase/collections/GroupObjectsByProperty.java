4d910a2f7fbdb12439ae0b3d67ae2b4a5adb86ed
Abhishek Sharma <abhiparashar8630@gmail.com>
2026-01-08 11:22:13 +0530

    [Abhishek Sharma] Add. Added The Collection framework program in Collections folder.

--- generics-practice/gcr-codebase/collections/GroupObjectsByProperty.java
+++ generics-practice/gcr-codebase/collections/GroupObjectsByProperty.java
@@ -6,6 +6,7 @@ public class GroupObjectsByProperty {
     public static void main(String[] args) {
         // Creating object of List class
         List<Employee> employees = new ArrayList<>();
+
         // Creating object of Employee class
         Employee employee1 = new Employee("Alice","HR");
         Employee employee2 = new Employee("Bob","IT");
@@ -14,14 +15,34 @@ public class GroupObjectsByProperty {
         employees.add(employee1);
         employees.add(employee2);
         employees.add(employee3);
+
+        //
+        HashMap<String,List<Employee>> map = GroupObjects(employees);
+        for(String key : map.keySet()){
+            System.out.print(key+": ");
+            System.out.print("[");
+            for(int i = 0; i < map.get(key).size(); i++) {
+                System.out.print(map.get(key).get(i).name);
+                if(i != map.get(key).size()-1) System.out.print(",");
+            }
+            System.out.print("], ");
+        }
     }
+
+
     // This method groups List of employee objects by their department using a Map<Department, List<Employee>>
     public static HashMap<String,List<Employee>> GroupObjects(List<Employee> employees){
         HashMap<String,List<Employee>> map = new HashMap<>();
         for(Employee employee : employees){
-            if(map.contains(employee.department))
+            if(map.containsKey(employee.department)){
+                map.get(employee.department).add(employee);
+            }
+            else{
+                map.put(employee.department,new ArrayList<>());
+                map.get(employee.department).add(employee);
+            }
         }
-
+        return map;
     }
 }
 
