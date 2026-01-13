8cf34106af4aedc97e2a1284c79a9fa193f4e085
Abhishek Sharma <abhiparashar8630@gmail.com>
2026-01-13 23:49:27 +0530

    [Abhishek Sharma] Add. Added All solved junit problems.

---
+++ stream-practice/gcr-codebase/junit/string_utils/StringUtilsTest.java
@@ -0,0 +1,24 @@
+package junit.string_utils;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringUtilsTest {
+ StringUtils utils = new StringUtils();
+
+ @Test
+ void testReverse() {
+     assertEquals("cba", utils.reverse("abc"));
+ }
+
+ @Test
+ void testPalindrome() {
+     assertTrue(utils.isPalindrome("madam"));
+     assertFalse(utils.isPalindrome("hello"));
+ }
+
+ @Test
+ void testToUpperCase() {
+     assertEquals("HELLO", utils.toUpperCase("hello"));
+ }
+}
