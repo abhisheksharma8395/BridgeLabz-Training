ac4ecff2ea2c991f9701315309fb309c979db10f
Abhishek Sharma <abhiparashar8630@gmail.com>
2026-01-20 14:52:15 +0530

    [Abhishek Sharma] Added the Leetcode problem Palidrome Linked List.

--- stream-practice/leet-code-codebase/PalindromeLinkedList.java
+++ stream-practice/leet-code-codebase/PalindromeLinkedList.java
@@ -1,35 +1,36 @@
 public class PalindromeLinkedList {
-    class Solution {
-        // This Method reverse the String
-        public Node reverse(Node head){
-            Node prev=null;
-            Node current=head;
-            while(current!=null){
-                Node agla=current.next;
-                current.next=prev;
-                prev=current;
-                current=agla;
-            }
-            return prev;
+
+    // This Method reverse the String
+    public Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node agla = current.next;
+            current.next = prev;
+            prev = current;
+            current = agla;
         }
-        // This Method is Used to check linked list is palindrome or not
-        public boolean isPalindrome(Node head) {
-            Node fast=head;
-            Node slow=head;
-            while(fast.next!=null && fast.next.next!=null){
-                fast=fast.next.next;
-                slow=slow.next;
-            }
-            Node temp=reverse(slow.next);
-            slow.next=temp;
-            Node start=head;
-            Node other=slow.next;
-            while(other!=null){
-                if(start.val!=other.val) return false;
-                start=start.next;
-                other=other.next;
-            }
-            return true;
-        }
+        return prev;
     }
+
+    // This Method is Used to check linked list is palindrome or not
+    public boolean isPalindrome(Node head) {
+        Node fast = head;
+        Node slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        Node temp = reverse(slow.next);
+        slow.next = temp;
+        Node start = head;
+        Node other = slow.next;
+        while (other != null) {
+            if (start.val != other.val) return false;
+            start = start.next;
+            other = other.next;
+        }
+        return true;
+    }
+
 }
