e52f8d91077874fded7833a25e30d1e3cfd7da3e
Abhishek Sharma <abhiparashar8630@gmail.com>
2026-01-21 20:54:53 +0530

    [Abhishek Sharma] Add. Added the CSV Data Handling solved problems

---
+++ io-programming-practice/gcr-codebase/csv_data_handling/SortCSVData.java
@@ -0,0 +1,54 @@
+package csv_data_handling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortCSVData {
+    public static void main(String[] args) {
+        String filePath = "io-programming-practice/gcr-codebase/csv_data_handling/employeeDetails.csv";
+        List<Employee> ansList = new ArrayList<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))){
+            // To skip the header
+            String line = bufferedReader.readLine();
+            while((line = bufferedReader.readLine())!=null){
+                String[] columns = line.split(",");
+                int employeeId = Integer.parseInt(columns[0]);
+                String employeeName = columns[1];
+                String department = columns[2];
+                int salary = Integer.parseInt(columns[3]);
+                ansList.add(new Employee(employeeId,employeeName,department,salary));
+            }
+            Collections.sort(ansList, (a,b) -> b.salary - a.salary);
+            for(int i = 0;i<5;i++){
+                ansList.get(i).display();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
+
+class Employee{
+    int employeeId;                                // Stores the employee Id
+    String employeeName;                           // Stores the employee name
+    String department;                             // Stores the department of employee
+    int salary;                                    // Stores the salary of employee
+
+    // Constructor of Employee class
+    public Employee(int employeeId,String employeeName,String department,int salary){
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public void display(){
+        System.out.println("ID : "+this.employeeId+", Name : "+this.employeeName+", Department : "+this.department+", Salary : "+this.salary);
+    }
+
+}
