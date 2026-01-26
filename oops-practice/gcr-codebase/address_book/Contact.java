e31186db43b6d210918399e76c6e9391a7abe701
Abhishek Sharma <abhiparashar8630@gmail.com>
2026-01-26 22:04:18 +0530

    [Abhishek Sharma] Add. Added the Solved problem Address Book Problem.

---
+++ oops-practice/gcr-codebase/address_book/Contact.java
@@ -0,0 +1,22 @@
+package address_book;
+
+public class Contact {
+    String firstName;                                                             // Stores the First Name of contact
+    String lastName;                                                              // Stores the Last Name of contact
+    String address;                                                               // Stores the Address of contact
+    String city;                                                                  // Stores the City Name of contact
+    String state;                                                                 // Stores the State Name of contact
+    String phoneNumber;                                                           // Stores the PhoneNumber of contact
+    String email;                                                                 // Stores the Email of contact
+
+    // Constructor of Contact class to initialize instance variables
+    public Contact(String firstName,String lastName,String address,String city,String state,String phoneNumber,String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
