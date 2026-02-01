9b1a5373878df5d46701c93be1c88d344ed74127
Abhishek Sharma <abhiparashar8630@gmail.com>
2026-02-01 23:02:07 +0530

    [Abhishek Sharma] Add. Added the Collectors questions .

--- java-8-features-practice/gcr-codebase/collectors/student_result_grouping/Main.java
+++ java-8-features-practice/gcr-codebase/collectors/student_result_grouping/Main.java
@@ -1,4 +1,11 @@
 package collectors.student_result_grouping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+        Student student1
+    }
 }
--- java-8-features-practice/gcr-codebase/collectors/student_result_grouping/Student.java
+++ java-8-features-practice/gcr-codebase/collectors/student_result_grouping/Student.java
@@ -1,4 +1,17 @@
 package collectors.student_result_grouping;
 
 public class Student {
+    String name;                                     // Stores the name of the student
+    String grades;                                   // Stores the grades of the student
+
+    // Constructor of Student class to initialize the instance variable
+    public Student(String name, String grades){
+        this.name = name;
+        this.grades = grades;
+    }
+
+    @Override
+    public String toString(){
+        return "Name : "+this.name+", Grades : "+this.grades;
+    }
 }
